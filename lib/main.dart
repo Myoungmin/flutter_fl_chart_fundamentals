edeba5820aabf54f8c1de447865134d9d5291eaf
AzureAD\고명민 <miung222@gmail.com>
2024-06-18 23:16:01 +0900

    windowLevel 함수 추가

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,39 @@ class ImageDataNotifier extends Notifier<ImageData> {
   void setImageData(ImageData info) {
     state = info;
   }
+
+  void windowLevel(int w1, int w2) {
+    final image = state.image;
+    final width = state.width;
+    final height = state.height;
+
+    if (w1 == w2) w2++;
+
+    final windowRange = w2 - w1;
+    const lutSize = 65536;
+    final lutWindowLevel = Uint8List(lutSize);
+
+    for (int rawValue = 0; rawValue < lutSize; rawValue++) {
+      final rawValueInRange = max(w1, min(rawValue, w2));
+      final byteVal = ((rawValueInRange - w1) * 255 ~/ windowRange);
+      lutWindowLevel[rawValue] = byteVal;
+    }
+
+    final displayImage = Uint8List(width * height * 3);
+
+    for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+        final rawValue = image![y * width + x];
+        final value = lutWindowLevel[rawValue];
+        final index = (y * width + x) * 3;
+        displayImage[index] = value;
+        displayImage[index + 1] = value;
+        displayImage[index + 2] = value;
+      }
+    }
+
+    state = state.copyWith(displayImage: displayImage);
+  }
 }
 
 class BarChartPage extends ConsumerWidget {
