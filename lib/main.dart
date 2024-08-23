7693bd4c7de3ef61a1300a30e548f7ec478217c3
AzureAD\고명민 <miung222@gmail.com>
2024-08-23 23:49:50 +0900

    스크롤로 barWidth scale 조절

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_fl_chart_fundamentals/image_data.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -62,118 +63,133 @@ class ImageDataNotifier extends Notifier<ImageData> {
   }
 }
 
+final scaleFactorProvider = StateProvider<double>((ref) => 1.0);
+
 class BarChartPage extends ConsumerWidget {
   const BarChartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     ImageData imageData = ref.watch(imageDataProvider);
+    double scaleFactor = ref.watch(scaleFactorProvider);
 
     return Center(
       child: LayoutBuilder(
         builder: (context, constraints) {
           double availableWidth = constraints.maxWidth;
 
-          return SizedBox(
-            width: availableWidth,
-            height: constraints.maxHeight,
-            child: BarChart(
-              BarChartData(
-                barGroups: _generateBarGroups(imageData, availableWidth),
-                titlesData: FlTitlesData(
-                  bottomTitles: AxisTitles(
-                    sideTitles: SideTitles(
-                      showTitles: true,
-                      reservedSize: 22,
-                      interval: 2048,
-                      getTitlesWidget: (double value, TitleMeta meta) {
-                        const style = TextStyle(
-                          color: Color(0xff7589a2),
-                          fontWeight: FontWeight.bold,
-                          fontSize: 10,
-                        );
+          return Listener(
+            onPointerSignal: (event) {
+              if (event is PointerScrollEvent) {
+                double scaleFactor = event.scrollDelta.dy > 0 ? 0.9 : 1.1;
+                ref.read(scaleFactorProvider.notifier).update((state) {
+                  double newScale = state * scaleFactor;
+                  return newScale;
+                });
+              }
+            },
+            child: SizedBox(
+              width: availableWidth,
+              height: constraints.maxHeight,
+              child: BarChart(
+                BarChartData(
+                  barGroups: _generateBarGroups(
+                      imageData, availableWidth, scaleFactor),
+                  titlesData: FlTitlesData(
+                    bottomTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                        showTitles: true,
+                        reservedSize: 22,
+                        interval: 2048,
+                        getTitlesWidget: (double value, TitleMeta meta) {
+                          const style = TextStyle(
+                            color: Color(0xff7589a2),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 10,
+                          );
 
-                        String text;
-                        int groupValue = value.toInt() * 256;
-                        if (groupValue % 2048 != 0) {
-                          text = '';
-                        } else if (groupValue >= 1000) {
-                          text = '${(groupValue / 1000).toStringAsFixed(0)}K';
-                        } else {
-                          text = groupValue.toString();
-                        }
+                          String text;
+                          int groupValue = value.toInt() * 256;
+                          if (groupValue % 2048 != 0) {
+                            text = '';
+                          } else if (groupValue >= 1000) {
+                            text = '${(groupValue / 1000).toStringAsFixed(0)}K';
+                          } else {
+                            text = groupValue.toString();
+                          }
 
-                        return SideTitleWidget(
-                          axisSide: meta.axisSide,
-                          space: 4,
-                          child: Text(text, style: style),
+                          return SideTitleWidget(
+                            axisSide: meta.axisSide,
+                            space: 4,
+                            child: Text(text, style: style),
+                          );
+                        },
+                      ),
+                    ),
+                    leftTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                        reservedSize: 40,
+                        showTitles: true,
+                        interval: 1000,
+                        getTitlesWidget: (double value, TitleMeta meta) {
+                          const style = TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 10,
+                          );
+
+                          String text;
+                          if (value.toInt() % 1000 != 0) {
+                            text = '';
+                          } else if (value >= 1000000) {
+                            text = '${(value / 1000000).toStringAsFixed(0)}M';
+                          } else if (value >= 1000) {
+                            text = '${(value / 1000).toStringAsFixed(0)}K';
+                          } else {
+                            text = value.toInt().toString();
+                          }
+
+                          return SideTitleWidget(
+                            axisSide: meta.axisSide,
+                            space: 4,
+                            child: Text(text, style: style),
+                          );
+                        },
+                      ),
+                    ),
+                    topTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                          showTitles: true,
+                          reservedSize: 88,
+                          getTitlesWidget: (double value, TitleMeta meta) {
+                            return const SizedBox.shrink();
+                          }),
+                    ),
+                    rightTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                          showTitles: true,
+                          getTitlesWidget: (double value, TitleMeta meta) {
+                            return const SizedBox.shrink();
+                          }),
+                    ),
+                  ),
+                  borderData: FlBorderData(
+                    show: true,
+                    border: const Border(
+                      top: BorderSide(color: Colors.transparent),
+                    ),
+                  ),
+                  gridData: const FlGridData(show: true),
+                  barTouchData: BarTouchData(
+                    enabled: true,
+                    touchTooltipData: BarTouchTooltipData(
+                      getTooltipItem: (group, groupIndex, rod, rodIndex) {
+                        return BarTooltipItem(
+                          'Count: ${rod.toY.toStringAsFixed(0)}\n Pixel Value: ${group.x * 256}\n',
+                          const TextStyle(color: Colors.red),
                         );
                       },
                     ),
-                  ),
-                  leftTitles: AxisTitles(
-                    sideTitles: SideTitles(
-                      reservedSize: 40,
-                      showTitles: true,
-                      interval: 1000,
-                      getTitlesWidget: (double value, TitleMeta meta) {
-                        const style = TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 10,
-                        );
-
-                        String text;
-                        if (value.toInt() % 1000 != 0) {
-                          text = '';
-                        } else if (value >= 1000000) {
-                          text = '${(value / 1000000).toStringAsFixed(0)}M';
-                        } else if (value >= 1000) {
-                          text = '${(value / 1000).toStringAsFixed(0)}K';
-                        } else {
-                          text = value.toInt().toString();
-                        }
-
-                        return SideTitleWidget(
-                          axisSide: meta.axisSide,
-                          space: 4,
-                          child: Text(text, style: style),
-                        );
-                      },
-                    ),
-                  ),
-                  topTitles: AxisTitles(
-                    sideTitles: SideTitles(
-                        showTitles: true,
-                        reservedSize: 88,
-                        getTitlesWidget: (double value, TitleMeta meta) {
-                          return const SizedBox.shrink();
-                        }),
-                  ),
-                  rightTitles: AxisTitles(
-                    sideTitles: SideTitles(
-                        showTitles: true,
-                        getTitlesWidget: (double value, TitleMeta meta) {
-                          return const SizedBox.shrink();
-                        }),
-                  ),
-                ),
-                borderData: FlBorderData(
-                  show: true,
-                  border: const Border(
-                    top: BorderSide(color: Colors.transparent),
-                  ),
-                ),
-                gridData: const FlGridData(show: true),
-                barTouchData: BarTouchData(
-                  enabled: true,
-                  touchTooltipData: BarTouchTooltipData(
-                    getTooltipItem: (group, groupIndex, rod, rodIndex) {
-                      return BarTooltipItem(
-                        'Count: ${rod.toY.toStringAsFixed(0)}\n Pixel Value: ${group.x * 256}\n',
-                        const TextStyle(color: Colors.red),
-                      );
-                    },
                   ),
                 ),
               ),
@@ -185,7 +201,7 @@ class BarChartPage extends ConsumerWidget {
   }
 
   List<BarChartGroupData> _generateBarGroups(
-      ImageData data, double availableWidth) {
+      ImageData data, double availableWidth, double scaleFactor) {
     List<int> histogram = List.filled(65536, 0);
 
     for (var value in data.image!) {
@@ -193,7 +209,7 @@ class BarChartPage extends ConsumerWidget {
     }
 
     int groupSize = 256;
-    double barWidth = availableWidth / groupSize;
+    double barWidth = (availableWidth / groupSize) * scaleFactor;
 
     List<int> groupedHistogram = List.filled(groupSize, 0);
     for (int i = 0; i < 65536; i++) {
